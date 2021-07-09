88afbbec6bfe16e256bb26c8f057d586f9012e58
Matthew D Elmer <matthewelmer@tamu.edu>
2021-07-09 14:19:44 -0500

    Finished
    Simplest tool to get the job done for my coding style

--- h2cpp.py
+++ h2cpp.py
@@ -26,6 +26,7 @@ type ClassName::funcName(params) isConst?
 
 To do:
 - header guard check?
+- maybe make end of declaration checking smarter?
 """
 
 
@@ -38,17 +39,64 @@ if len(sys.argv) != 2:
 
 # We know the class name from the header file name
 className = sys.argv[1][:-2]
-# print(f"className: {className}")
+# print(f"className: '{className}'")
 
 
 # Open files
 hfile = open(sys.argv[1], 'r')
 cppfile = open(className + ".cpp", 'w')
 
+cppfile.write(f"#include \"{sys.argv[1]}\"\n")
+
+inDefinition = False
+for line in hfile:
+    # discard lines before class definition
+    if not inDefinition:
+        if f"class {className} {{" not in line:
+            continue
+        else:
+            inDefinition = True
+            continue
+    
+    # strip that hoe
+    line = line.strip()
+    
+    # stop reading when end of definition reached
+    if line == "};":
+        break
+
+    # strip comments
+    if "//" in line:
+        line = line[:line.index("//")]
+        line = line.strip()
+
+    # Don't read private/public decorators
+    if "private" in line or "public" in line:
+        continue
+
+    # skip empty lines
+    if line == "":
+        continue
+    
+    # skip non-function-declaration lines
+    if "(" not in line and ")" not in line:
+        continue
+    
+    if " " in line and line.index(" ") < line.index("("):  # If there's a space in the line
+        theType = line[:line.index(" ")]
+        theRest = line[line.index(" ") + 1:]
+        conversion = f"{theType} {className}::{theRest[:-1]}\n{{\n    \n}}\n"
+    else:  # It's one of the big five
+        conversion = f"{className}::{line[:-1]}\n{{\n\n}}\n"
+    
+    print("Converting:\n"
+    f"{line}\n"
+    "into:\n"
+    f"{conversion}")
+
+    cppfile.write("\n" + conversion)
+    
 
 # Close files
 hfile.close()
 cppfile.close()
-            
-
-# skip until class declaration
